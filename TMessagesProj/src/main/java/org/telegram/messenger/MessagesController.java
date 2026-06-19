1ba3f7aba6da94a83fd6155e985e45199cab2ddd
00 <shamsaver1@gmail.com>
2026-06-19 19:56:50 +0300

    fix: restore editingMessagesFinal declaration before lambda + add R.string.Messages

--- TMessagesProj/src/main/java/org/telegram/messenger/MessagesController.java
+++ TMessagesProj/src/main/java/org/telegram/messenger/MessagesController.java
@@ -18632,6 +18632,7 @@ public class MessagesController extends BaseController implements NotificationCe
                     }
                 }
             }
+            LongSparseArray<ArrayList<MessageObject>> editingMessagesFinal = editingMessages;
             getMessagesStorage().getStorageQueue().postRunnable(() -> AndroidUtilities.runOnUIThread(() -> {
                 getNotificationsController().processEditedMessages(editingMessagesFinal);
                 getTopicsController().processEditedMessages(editingMessagesFinal);
