f4ce7a9ce816795c469df3fd68c2a108ab32b54b
00 <shamsaver1@gmail.com>
2026-06-23 11:15:24 +0300

    fix: restore ActionBarMenuSubItem.java from 530ec3d (batch rollback of bad merge artifacts)

--- TMessagesProj/src/main/java/org/telegram/ui/ActionBar/ActionBarMenuSubItem.java
+++ TMessagesProj/src/main/java/org/telegram/ui/ActionBar/ActionBarMenuSubItem.java
@@ -44,10 +44,9 @@ public class ActionBarMenuSubItem extends FrameLayout {
 
     private int textColor;
     private int iconColor;
-    private PorterDuff.Mode iconColorMode;
     private int selectorColor;
 
-    int selectorRad = 12;
+    int selectorRad = 6;
     boolean top;
     boolean bottom;
 
@@ -80,7 +79,6 @@ public class ActionBarMenuSubItem extends FrameLayout {
 
         textColor = getThemedColor(Theme.key_actionBarDefaultSubmenuItem);
         iconColor = getThemedColor(Theme.key_actionBarDefaultSubmenuItemIcon);
-        iconColorMode = PorterDuff.Mode.MULTIPLY;
         selectorColor = getThemedColor(Theme.key_dialogButtonSelector);
 
         updateBackground();
@@ -108,7 +106,7 @@ public class ActionBarMenuSubItem extends FrameLayout {
         if (needCheck > 0) {
             checkView = new CheckBox2(getContext(), 26, resourcesProvider);
             checkView.setDrawUnchecked(false);
-            checkView.setColor(-1, -1, Theme.key_actionBarDefaultSubmenuItem);
+            checkView.setColor(-1, -1, Theme.key_radioBackgroundChecked);
             checkView.setDrawBackgroundAsArc(-1);
             if (needCheck == 1) {
                 checkViewLeft = !LocaleController.isRTL;
@@ -241,20 +239,6 @@ public class ActionBarMenuSubItem extends FrameLayout {
         backupImageView.setImage(imageLocation, imageFilter, thumb, parentObject);
     }
 
-
-    public void setIconColorImage(int iconColor) {
-        if (backupImageView != null) {
-            backupImageView.setColorFilter(new PorterDuffColorFilter(iconColor, PorterDuff.Mode.SRC_IN));
-        }
-    }
-
-    public void setImageSize(int widthDp, int heightDp) {
-        if (backupImageView != null) {
-            backupImageView.setLayoutParams(LayoutHelper.createFrame(widthDp, heightDp, Gravity.CENTER_VERTICAL | (LocaleController.isRTL ? Gravity.RIGHT : Gravity.LEFT)));
-        }
-    }
-
-
     public ActionBarMenuSubItem setColors(int textColor, int iconColor) {
         setTextColor(textColor);
         setIconColor(iconColor);
@@ -268,12 +252,8 @@ public class ActionBarMenuSubItem extends FrameLayout {
     }
 
     public void setIconColor(int iconColor) {
-        setIconColor(iconColor, PorterDuff.Mode.MULTIPLY);
-    }
-
-    public void setIconColor(int iconColor, PorterDuff.Mode mode) {
-        if (this.iconColor != iconColor || this.iconColorMode != mode) {
-            imageView.setColorFilter(new PorterDuffColorFilter(this.iconColor = iconColor, this.iconColorMode = mode));
+        if (this.iconColor != iconColor) {
+            imageView.setColorFilter(new PorterDuffColorFilter(this.iconColor = iconColor, PorterDuff.Mode.MULTIPLY));
         }
     }
 
